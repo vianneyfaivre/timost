20f8f19db0064d07039259ce5ab8b38c4d2c5e1e
Vianney FAIVRE <vianney.faivre@gmail.com>
2020-12-09 18:06:24 +1300

    support years. moved classes to proper model file

--- src/App.svelte
+++ src/App.svelte
@@ -1,96 +1,37 @@
 <script lang="ts">
-	const WORK_HOURS_PER_WEEK: number = 37.5;
-	const WORK_DAYS_PER_WEEK: number = 5;
-	const WEEKS_PER_MONTH: number = 4;
+import { Duration, DurationUnit } from "./model/Duration";
 
-	const WORK_HOURS_PER_MONTH: number = WORK_HOURS_PER_WEEK * WEEKS_PER_MONTH;
-	const WORK_HOURS_PER_DAY: number = WORK_HOURS_PER_WEEK / WORK_DAYS_PER_WEEK;
+const WORK_HOURS_PER_WEEK: number = 37.5;
+const WORK_DAYS_PER_WEEK: number = 5;
+const WEEKS_PER_MONTH: number = 4;
 
-	let thingName: string = 'thing';
-	let monthlyIncome: number = 0;
-	let itemCost: number = 0;
-	let result: Duration;
-	let hourlyIncome: number;
-	let init: boolean = false;
+const WORK_HOURS_PER_MONTH: number = WORK_HOURS_PER_WEEK * WEEKS_PER_MONTH;
+const WORK_HOURS_PER_DAY: number = WORK_HOURS_PER_WEEK / WORK_DAYS_PER_WEEK;
 
-	class Duration {
+// input
+let thingName: string = 'thing';
+let monthlyIncome: number = 0;
+let itemCost: number = 0;
 
-		value: number;
-		unit: DurationUnit;
-		valid: boolean;
-	}
+// output
+let result: Duration;
+let hourlyIncome: number;
+let showResults: boolean = false;
 
-	enum DurationUnit {
-		SECONDS = "seconds",
-		MINUTES = "minutes",
-		HOURS = "hours",
-		DAYS = "days",
-		MONTHS = "months",
-		YEARS = "years"
-	}
-
-	// TODO: Move to Duration#constructor
-	function getDuration(hours: number): Duration {
-		let value: number;
-		let unit: DurationUnit;
-
-		// Seconds or minutes
-		if(hours <= 1) {
-
-			const minutes: number = hours * 60;
-
-			if(+(minutes.toFixed(0)) > 0) {
-				value = Math.ceil(minutes);
-				unit = DurationUnit.MINUTES;
-			} else {
-				value = Math.ceil(minutes * 60);
-				unit = DurationUnit.SECONDS;
-			}
-		}
-		// Hours
-		else if (hours < WORK_HOURS_PER_DAY) {
-			value = Math.ceil(hours)
-			unit = DurationUnit.HOURS;
-		}
-		// Days, months or years
-		else {
-
-			const days = +(hours / WORK_HOURS_PER_DAY).toFixed(1);
-
-			// Days
-			if(days <= 30) {
-
-				value = days;
-				unit = DurationUnit.DAYS;
-			} 
-			// Months
-			else {
-
-				value = +(days / 30).toFixed(2);
-				unit = DurationUnit.MONTHS;
-			}
-		}
-
-		return {
-			value: value,
-			unit: unit,
-			valid: value > 0 && value != Infinity
-		};
-	}
-
-	$: {
-		hourlyIncome = +(monthlyIncome / WORK_HOURS_PER_MONTH).toFixed(2);
-		const hours = itemCost / hourlyIncome;
-		result = getDuration(hours);
-		init = monthlyIncome > 0 && itemCost > 0;
-	}
+// Bound elements
+$: {
+	hourlyIncome = +(monthlyIncome / WORK_HOURS_PER_MONTH).toFixed(2);
+	const hours = itemCost / hourlyIncome;
+	result = new Duration(hours, WORK_HOURS_PER_DAY);
+	showResults = monthlyIncome > 0 && itemCost > 0;
+}
 
 </script>
 
 <style>
-	input[type=number] {
-		width: 7em;
-	}
+input[type=number] {
+	width: 7em;
+}
 </style>
 
 <h1>How long do you need to work to afford that {thingName}?</h1>
@@ -104,7 +45,7 @@
 	<input bind:value={itemCost} type=number min=1 step=100 /> $
 </p>
 
-{#if init === true}
+{#if showResults === true}
 	<h2>Results</h2>
 
 	{#if result.valid}
---
+++ src/model/Duration.ts
@@ -0,0 +1,70 @@
+export enum DurationUnit {
+    SECONDS = "seconds",
+    MINUTES = "minutes",
+    HOURS = "hours",
+    DAYS = "days",
+    MONTHS = "months",
+    YEARS = "years"
+}
+
+export class Duration {
+
+    value: number;
+    unit: DurationUnit;
+    valid: boolean;
+
+    constructor(hours: number, workHoursPerDay: number) {
+        let value: number;
+        let unit: DurationUnit;
+    
+        // Seconds or minutes
+        if(hours <= 1) {
+    
+            const minutes: number = hours * 60;
+    
+            if(+(minutes.toFixed(0)) > 0) {
+                value = Math.ceil(minutes);
+                unit = DurationUnit.MINUTES;
+            } else {
+                value = Math.ceil(minutes * 60);
+                unit = DurationUnit.SECONDS;
+            }
+        }
+        // Hours
+        else if (hours < workHoursPerDay) {
+            value = Math.ceil(hours)
+            unit = DurationUnit.HOURS;
+        }
+        // Days, months or years
+        else {
+    
+            const days = +(hours / workHoursPerDay).toFixed(1);
+            const months = +(days / 30).toFixed(2);
+    
+            // Days
+            if(days <= 30) {
+    
+                value = days;
+                unit = DurationUnit.DAYS;
+            } 
+            // Months
+            else if (months < 12) {
+    
+                value = months;
+                unit = DurationUnit.MONTHS;
+            } 
+            // Years
+            else {
+                
+                value = +(months / 12).toFixed(1);
+                unit = DurationUnit.YEARS;
+            }
+        }
+    
+        return {
+            value: value,
+            unit: unit,
+            valid: value > 0 && value != Infinity
+        };
+    }
+}
